0dcb309504541441657ee75963ad8521011a6e75
Hassam Shahryar <hassamytchannel77@gmail.com>
2025-08-02 01:14:40 +0100

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ export default function Hero() {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.6, duration: 1, ease: "easeOut" }}
             viewport={{ once: true }}
-            className="absolute md:top-24 top-16 z-20 text-white px-7 w-full space-y-3"
+            className="absolute md:top-20 top-16 z-20 text-white px-7 w-full space-y-3"
           >
             <span className="z-10 rounded-md inline-block px-2 py-1 bg-indigo-600 text-white font-bold italic skew-x-[-10deg] text-center text-[13px] md:text-[15px]">
               <h2 className="skew-x-[10deg] tracking-wide capitalize">
